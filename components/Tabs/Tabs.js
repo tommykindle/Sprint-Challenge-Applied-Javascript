4a71c571b418c335a5a6b0345df03ff181a7d815
Tommy <tommykindle83@gmail.com>
2019-06-21 09:02:46 -0700

    selectTab method complete working on TabCard class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -17,7 +17,7 @@ class TabLink {
       // else if `all` is false, only select the cards with matching this.tabData values
       // this.cards = ;
     }
-     <- Delete this comment block when you work on the if statement
+    // <- Delete this comment block when you work on the if statement
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     // this.cards = Array.from(this.cards).map();
@@ -38,20 +38,20 @@ class TabLink {
      const cards = document.querySelectorAll('.card'); 
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    Array.from(cards).forEach(card => card.classList.style.display ='none'); 
     
     // Add a class of ".active-tab" to this.tabElement
-    // this.tabElement;
+     this.tabElement.classList.add('active-tab'); 
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+     this.cards.forEach(card => card.selectCard());
   }
 }
 
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+     this.cardElement = cardElement; 
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
